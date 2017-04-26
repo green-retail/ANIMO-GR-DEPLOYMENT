5eb344a6b1ba9943ab5b67f049387a3a1d9c01a3
Keinier Caboverde <keinier@gmail.com>
2017-04-26 10:29:51 -0400

    se cambio la ip del servidor a 10.20.0.2 en la replicacion dimep

--- dimepsync.cmd
+++ dimepsync.cmd
@@ -3,7 +3,7 @@ title Green Retail DIMEP DAEMON
 
 :loop
 cls
-"DIMEP Sync.exe" "Server=10.20.0.4;Database=DMPACESSOII;User Id=sa;Password=sa;"
+"DIMEP Sync.exe" "Server=10.20.0.2;Database=DMPACESSOII;User Id=sa;Password=administrador;"
 timeout 360
 cls
 goto loop
